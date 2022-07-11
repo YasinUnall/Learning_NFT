5c6d6dfa88f2bf892859aa9e32ae84d733dc2347
YasinUnall <yasinunalnn@gmail.com>
2022-07-11 20:43:08 +0300

    new images added plus brownie config changes have been made

--- tests/unit/test_simple_collectible.py
+++ tests/unit/test_simple_collectible.py
@@ -1,6 +1,6 @@
 import pytest
 from brownie import network
-from scripts.deploy_and_create import deploy_and_create
+from scripts.simple_collectible.deploy_and_create import deploy_and_create
 from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 import pytest
 
